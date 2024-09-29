c9b10d2cc55d01d2c7f078798a9ce20739dd4c06
Bhagirath Saxena <72344025+rix4uni@users.noreply.github.com>
2024-09-30 02:03:55 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
     "fmt"
     "io/ioutil"
     "net/http"
+    "time"
 )
 
 type RSS struct {
@@ -52,6 +53,66 @@ func main() {
         "https://medium.com/feed/tag/security",
         "https://medium.com/feed/tag/vulnerability",
         "https://medium.com/feed/tag/cybersecurity",
+        "https://medium.com/feed/tag/penetration-testing",
+        "https://medium.com/feed/tag/hacking",
+        "https://medium.com/feed/tag/information-technology",
+        "https://medium.com/feed/tag/infosec",
+        "https://medium.com/feed/tag/web-security",
+        "https://medium.com/feed/tag/bug-bounty-tips",
+        "https://medium.com/feed/tag/bugs",
+        "https://medium.com/feed/tag/pentesting",
+        "https://medium.com/feed/tag/xss-attack",
+        "https://medium.com/feed/tag/information-security",
+        "https://medium.com/feed/tag/cross-site-scripting",
+        "https://medium.com/feed/tag/hackerone",
+        "https://medium.com/feed/tag/bugcrowd",
+        "https://medium.com/feed/tag/bugbounty-writeup",
+        "https://medium.com/feed/tag/bug-bounty-writeup",
+        "https://medium.com/feed/tag/bug-bounty-hunter",
+        "https://medium.com/feed/tag/bug-bounty-program",
+        "https://medium.com/feed/tag/ethical-hacking",
+        "https://medium.com/feed/tag/application-security",
+        "https://medium.com/feed/tag/google-dorking",
+        "https://medium.com/feed/tag/dorking",
+        "https://medium.com/feed/tag/cyber-security-awareness",
+        "https://medium.com/feed/tag/google-dork",
+        "https://medium.com/feed/tag/web-pentest",
+        "https://medium.com/feed/tag/vdp",
+        "https://medium.com/feed/tag/information-disclosure",
+        "https://medium.com/feed/tag/exploit",
+        "https://medium.com/feed/tag/vulnerability-disclosure",
+        "https://medium.com/feed/tag/web-cache-poisoning",
+        "https://medium.com/feed/tag/rce",
+        "https://medium.com/feed/tag/remote-code-execution",
+        "https://medium.com/feed/tag/local-file-inclusion",
+        "https://medium.com/feed/tag/vapt",
+        "https://medium.com/feed/tag/dorks",
+        "https://medium.com/feed/tag/github-dorking",
+        "https://medium.com/feed/tag/lfi",
+        "https://medium.com/feed/tag/vulnerability-scanning",
+        "https://medium.com/feed/tag/subdomain-enumeration",
+        "https://medium.com/feed/tag/cybersecurity-tools",
+        "https://medium.com/feed/tag/bug-bounty-hunting",
+        "https://medium.com/feed/tag/ssrf",
+        "https://medium.com/feed/tag/idor",
+        "https://medium.com/feed/tag/pentest",
+        "https://medium.com/feed/tag/file-upload",
+        "https://medium.com/feed/tag/file-inclusion",
+        "https://medium.com/feed/tag/security-research",
+        "https://medium.com/feed/tag/directory-listing",
+        "https://medium.com/feed/tag/log-poisoning",
+        "https://medium.com/feed/tag/cve",
+        "https://medium.com/feed/tag/xss-vulnerability",
+        "https://medium.com/feed/tag/shodan",
+        "https://medium.com/feed/tag/censys",
+        "https://medium.com/feed/tag/zoomeye",
+        "https://medium.com/feed/tag/recon",
+        "https://medium.com/feed/tag/xss-bypass",
+        "https://medium.com/feed/tag/bounty-program",
+        "https://medium.com/feed/tag/subdomain-takeover",
+        "https://medium.com/feed/tag/bounties",
+        "https://medium.com/feed/tag/api-key",
+        "https://medium.com/feed/tag/cyber-sec",
     }
 
     for _, url := range urls {
@@ -71,5 +132,8 @@ func main() {
         }
 
         fmt.Println() // Add a blank line between sections
+
+        // Sleep for 2 seconds before fetching the next URL
+        time.Sleep(2 * time.Second)
     }
 }
